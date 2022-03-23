1a1eaf02fe4a5eb8dc2a8732d4b1ebb0a9a4801b
Wictor Chaves <wictor@hotmail.com.br>
2022-03-23 10:27:34 -0300

    Primeiro commit

---
+++ js/generate-name.js
@@ -0,0 +1,65 @@
+function generateName() {
+
+    var inputDescription = $('#description');
+    var inputDate        = $('#date');
+    var inputTime        = $('#time');
+
+    var outputName         = $('#sql-name');
+    var outputNameRollback = $('#sql-name-rollback');
+
+    var date        = inputDate.val().replace(/-/g, '');
+    var time        = inputTime.val().replace(/:/g, '');
+    var description = inputDescription.val().replace(/[^\w\s]/gi, '').replace(/\s/g, '');
+
+    var today = new Date();
+
+    if (date.length == 0) {
+        date = today.getFullYear() + '' + (today.getMonth() + 1) + '' + today.getDate();
+    }
+
+    if (time.length == 0) {
+        time = today.getHours() + '' + today.getMinutes();
+    }
+
+    var name          = `V${date}.${time}__${description}.sql`;
+    var rollback_name = `V${date}.${time}__ROLLBACK_${description}.sql`;
+
+    outputName.text(name);
+    outputNameRollback.text(rollback_name);
+}
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+$(function () {
+
+    $('#btn-generate').click(function () {
+        generateName();
+    });
+
+    $('#description').keyup(function () {
+        generateName();
+    });
+
+    $('#date').change(function () {
+        generateName();
+    });
+
+    $('#time').change(function () {
+        generateName();
+    });
+
+    $('#sql-name').click(function () {
+        copyToClipboard('#sql-name');
+    });
+
+    $('#sql-name-rollback').click(function () {
+        copyToClipboard('#sql-name-rollback');
+    });
+
+});
